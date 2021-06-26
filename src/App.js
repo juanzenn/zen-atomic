edbedf360facc9b30beefa8c49dd7e16eb4e2d8d
juanzenn <juanandres140299@gmail.com>
2021-06-25 21:02:33 -0400

    Cleanup off main branch

--- src/App.js
+++ src/App.js
@@ -2,22 +2,10 @@ import Text from './components/Text/Text'
 
 function App() {
   return (
-    <>
-      <Text tag='h1'>Text</Text>
-      <Text tag='h2'>Text</Text>
-      <Text tag='h3'>Text</Text>
-      <Text tag='h4'>Text</Text>
-      <Text tag='h5'>Text</Text>
-      <Text tag='h6'>Text</Text>
-      <Text tag='base'>Text</Text>
-      <Text tag='small'>Text</Text>
-
-      <section className='px-4 w-scree h-screen bg-green-400 flex flex-col justify-center items-center text-center'>
-        <Text tag='h1'>Hello World</Text>
-        <Text tag='h3'>This is my first real library (question mark)</Text>
-        <Text>The big idea of the Text component is to make a reusable tag with reusable styles since Tailwind doesn't have any default</Text>
-      </section>
-    </>
+    <section className='flex flex-col  justify-center items-center w-screen h-screen bg-green-200'>
+      <h1 className='font-bold text-9xl'>Hello World!</h1>
+      <p className='font-bold text-5xl'>There is not yet any component here!</p>
+    </section>
 
   );
 }
